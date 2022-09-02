96aff7c35fc0d3c331f9fff7475a700bcbde1224
Yimesgen Morka <dochomorka@gmail.com>
2022-09-02 09:15:24 +0300

    cleanup code

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -150,17 +150,6 @@ def create_app(test_config=None):
             db.session.close()
         return jsonify(data)
 
-    """
-    @TODO:
-    Create a POST endpoint to get questions to play the quiz.
-    This endpoint should take category and previous question parameters
-    and return a random questions within the given category,
-    if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
-    """
     @app.route('/quizzes', methods=['POST'])
     def quizzes():
         prev_questions = request.json['questions']
